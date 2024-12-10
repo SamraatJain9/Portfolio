428431c6e8fffaa12b7ac6e488c834b8f13f1e78
Samraat-LK789 <s.jain9@newcastle.ac.uk>
2024-12-10 15:33:08 +0530

    adding links to other pages

--- app.py
+++ app.py
@@ -6,6 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/companies')
+def company():
+    return render_template('companies.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def project():
+    return render_template('projects.html')
+
+@app.route('/nonprofits')
+def other():
+    return render_template('nonprofits.html')
+
+@app.route('/reserach')
+def research():
+    return render_template('research.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
